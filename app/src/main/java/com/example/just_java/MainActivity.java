22de5758aad13f62d7d1074621b6016eb027db93
timothy <timothyabiodun300@gmail.com>
2020-07-26 15:39:23 +0100

    Project

--- app/src/main/java/com/example/just_java/MainActivity.java
+++ app/src/main/java/com/example/just_java/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.just_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mScore = 0;
+    private int mScoreB = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void displayFreeThrowB(View view){
+        mScoreB += 1;
+        TextView t = findViewById(R.id.team_b_score);
+        t.setText(String.valueOf(mScoreB));
+    }
+
+    public void displayThreeBPoints(View view){
+        mScoreB += 3;
+        TextView t = findViewById(R.id.team_b_score);
+        t.setText(String.valueOf(mScoreB));
+    }
+
+
+    public void displayTwoBPoints(View view){
+        TextView t = findViewById(R.id.team_b_score);
+        mScoreB += 2;
+        t.setText(String.valueOf(mScoreB));
+    }
+
     public void displayFreeThrow(View view){
+        mScore += 1;
         TextView t = findViewById(R.id.team_a_score);
         t.setText(String.valueOf(mScore));
     }
@@ -35,8 +58,11 @@ public class MainActivity extends AppCompatActivity {
     }
     public void reset(View view){
         TextView reset = (TextView) findViewById(R.id.team_a_score);
+        TextView t = (TextView) findViewById(R.id.team_b_score);
         mScore = 0;
+        mScoreB = 0;
         reset.setText(String.valueOf(mScore));
+        t.setText(String.valueOf(mScoreB));
     }
 
 }
